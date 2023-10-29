1112e8bf603a662b1da530f05adcd6166ee9dd99
miguelidevelop <miguelidevelop@gmail.com>
2023-10-29 12:58:31 +0000

    holi

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -12,10 +12,10 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username: string = '';
+  email: string = '';
   password: string = '';
   loading: boolean = false;
-  
+
   constructor(private toastr: ToastrService,
     private _userService: UserService,
     private router: Router,
@@ -27,14 +27,14 @@ export class LoginComponent implements OnInit {
   login() {
 
     // Validamos que el usuario ingrese datos
-    if (this.username == '' || this.password == '') {
+    if (this.email == '' || this.password == '') {
       this.toastr.error('Todos los campos son obligatorios', 'Error');
       return
     }
 
     // Creamos el body
     const user: User = {
-      username: this.username,
+      email: this.email,
       password: this.password
     }
 
