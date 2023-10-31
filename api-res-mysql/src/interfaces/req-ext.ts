5d47dca01848986b8680f2c1dee53760ec47887b
MigueliDeveloper <miguelidevelop@gmail.com>
2023-10-31 18:03:40 +0000

    HOLI

--- api-res-mysql/src/interfaces/req-ext.ts
+++ api-res-mysql/src/interfaces/req-ext.ts
@@ -1,6 +1,24 @@
 import { JwtPayload } from "jsonwebtoken";
 import { Request } from "express";
 
+/**  
+ * 
+ * TODO:
+ * 
+ * Interfaz: RequestExt
+ * 
+ * Propósito: Esta interfaz extiende la interfaz Request de Express para agregar un campo user que representa el usuario autenticado.
+ * 
+ * Importaciones:
+ * 
+ * JwtPayload: El tipo de datos que representa el payload de un token JWT.
+ * Request: La interfaz de Express que representa una solicitud HTTP.
+ * 
+ * Campos:
+ * user?: JwtPayload | { id: string }: El campo user es opcional y puede representar un objeto JwtPayload o un objeto con un campo id de tipo string.
+ * 
+ * */
+
 export interface RequestExt extends Request {
   user?: JwtPayload | { id: string };
 }
