c12efde5d3516c7d672b76d3f9f19c4f680a1b5e
miguelidevelop <miguelidevelop@gmail.com>
2023-11-01 14:23:30 +0000

    HOLI

--- src/app/componentes/contacto/contacto.component.ts
+++ src/app/componentes/contacto/contacto.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+// FormBuilder y FormGroup para crear y manejar formularios.
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+// ActivatedRoute y Router para obtener el ID del mensaje que se está editando.
 import {ActivatedRoute, Router} from '@angular/router'
+// ToastrService para mostrar notificaciones.
 import { ToastrService } from 'ngx-toastr'
 // Servicio - Modelo
+// GMensaje y GmensajeService para interactuar con el modelo de datos y el servicio de mensajes.
 import { GMensaje  } from '../../Models/GMensaje'
 import { GmensajeService} from '../../services/gmensajes.service'
 
@@ -12,6 +16,17 @@ import { GmensajeService} from '../../services/gmensajes.service'
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit{
+
+  /**
+   *
+   * TODO:
+   *
+   * El componente tiene las siguientes propiedades:
+   *
+   * contactoForm: El formulario que los usuarios utilizan para enviar mensajes.
+   * titulo: El título del formulario.
+   * id: El ID del mensaje que se está editando.
+   */
   contactoForm: FormGroup;
   titulo = 'Formulario de Contacto';
   id: string | null;
@@ -19,7 +34,7 @@ export class ContactoComponent implements OnInit{
               private router: Router,
               private toastr: ToastrService,
               private _mensajeService: GmensajeService,
-              private aRouter: ActivatedRoute) { 
+              private aRouter: ActivatedRoute) {
     this.contactoForm = this.fb.group({
       nombre: ['', Validators.required],
       email: ['', Validators.required],
@@ -28,9 +43,27 @@ export class ContactoComponent implements OnInit{
     })
     this.id = this.aRouter.snapshot.paramMap.get('id');
   }
+
+  /**
+   *
+   * TODO:
+   *
+   * ngOnInit(): Se llama cuando el componente se inicializa.
+   * En este método, se llama al método esEditar() para verificar si el componente está editando un mensaje existente.
+   */
   ngOnInit(): void {
     this.esEditar();
   }
+
+  /**
+   *
+   * TODO:
+   *
+   * agregarMensaje(): Se llama cuando el usuario envía el formulario.
+   * En este método, se crea un objeto GMensaje con los datos del formulario.
+   * Si el componente está editando un mensaje, se llama al método editarMensaje() del servicio de mensajes.
+   * De lo contrario, se llama al método guardarMensaje() del servicio de mensajes.
+   */
   agregarMensaje() {
     const MENSAJE: GMensaje  = {
       nombre: this.contactoForm.get('nombre')?.value,
@@ -59,6 +92,15 @@ export class ContactoComponent implements OnInit{
       })
     }
   }
+
+  /**
+   *
+   * TODO:
+   *
+   * esEditar(): Se llama para verificar si el componente está editando un mensaje existente.
+   * Si el componente está editando un mensaje, se establece el título del formulario en "Editar Mensaje" y se obtiene el mensaje del servicio de mensajes.
+   * Luego, los datos del mensaje se establecen en el formulario.
+   */
   esEditar() {
     if(this.id !== null) {
       this.titulo = 'Editar Mensaje';
